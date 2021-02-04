5466b009e6765d7d8bd02577d84a667c75a28057
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-02-04 16:20:58 +0100

    create main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,33 @@
 <template>
-  <div class="home">
-  </div>
+  <app-page title="Список Заявок">
+    <template #header>
+      <button class="btn primary" @click="modal = true">Create</button>
+    </template>
+
+    <request-table :requests="[]"></request-table>
+
+    <teleport to="body">
+      <app-modal v-if="modal" title="Создать заявку" @close="modal = false"></app-modal>
+    </teleport>
+
+  </app-page>
+
 </template>
 
 <script>
+import {ref} from 'vue'
+import RequestTable from '../components/request/RequestTable.vue'
+import AppPage from '../components/ui/AppPage'
+import AppModal from '../components/ui/AppModal'
+
 export default {
-  name: 'Home',
+  setup() {
+    const modal = ref(false)
+
+    return {
+      modal
+    }
+  },
+  components: { AppPage, RequestTable, AppModal }
 }
 </script>
