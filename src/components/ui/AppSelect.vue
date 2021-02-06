43305c4ce97b6be4533b5b740b631d47774823b1
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-02-06 18:11:10 +0100

    add dinamic filters

---
+++ src/components/ui/AppSelect.vue
@@ -0,0 +1,19 @@
+<template>
+  <select >
+        <option disabled selected>Выберите статус</option>
+        <option value="done">Завершен</option>
+        <option value="cancelled">Отменен</option>
+        <option value="active">Активен</option>
+        <option value="pending">Выполняется</option>
+      </select>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,8 @@
       <button class="btn primary" @click="modal = true">Create</button>
     </template>
 
+    <request-filter v-model="filter"></request-filter>
+
     <request-table :requests="requests"></request-table>
 
     <teleport to="body">
@@ -18,11 +20,12 @@
 </template>
 
 <script>
-import {ref, computed, onMounted} from 'vue'
+import {ref, computed, onMounted, watch} from 'vue'
 import { useStore } from 'vuex'
 import RequestTable from '../components/request/RequestTable.vue'
 import AppPage from '../components/ui/AppPage'
 import AppModal from '../components/ui/AppModal'
+import RequestFilter from '../components/request/RequestFilter'
 import AppLoader from '../components/ui//AppLoader'
 import RequestModal from '../components/request/RequestModal'
 
@@ -31,6 +34,9 @@ export default {
     const store = useStore()
     const modal = ref(false)
     const loading = ref(false)
+    const filter = ref({})
+
+   // watch(filter, val => console.log(val))
 
     onMounted(async () => {
       loading.value = true
@@ -38,13 +44,26 @@ export default {
       loading.value = false
     })
 
-    const requests = computed(() => store.getters['request/requests'])
+    const requests = computed(() => store.getters['request/requests']
+      .filter(request => {
+        if (filter.value.name) {
+          return request.fio.includes(filter.value.name)
+        }
+        return request
+        })
+      .filter(request => {
+        if (filter.value.status) {
+          return filter.value.status === request.status
+        }
+        return request
+        })
+      )
     
     return {
-      modal, requests, loading
+      modal, requests, loading, filter
     }
   },
  
-  components: { AppPage, RequestTable, AppModal, RequestModal , AppLoader}
+  components: { AppPage, RequestTable, AppModal, RequestModal , AppLoader, RequestFilter}
 }
 </script>
