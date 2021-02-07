e7b57e299d22619f1331fcb8ae7289a34cf10bdf
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-02-07 14:07:09 +0100

    add help page

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <app-page back title="Помощь">
+    <question-item 
+      v-for="q in questions" 
+      :key="q.title"
+      :question='q' 
+    >
+    </question-item>
+  </app-page>
 </template>
+<script>
+import AppPage from '../components/ui/AppPage'
+import QuestionItem from '../components/question/QuestionItem'
+
+export default {
+  components: {AppPage, QuestionItem},
+  setup(){
+    const questions= [
+      {title: 'Оформление заявки', text: 'Возможность формировать заявки со статусом, менять статус, удалять заявку'},
+      {title: 'Как работает авторизация', text: 'Все реализовано через Firebase'},
+    ]
+
+    return {questions, QuestionItem}
+  }
+}
+</script>
